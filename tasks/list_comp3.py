d37824d40ada9e1541f971a514fc6def0669f1f2
Jacob Coles <jacob@five2one.com.au>
2020-12-01 17:56:02 +0100

    Modified list_comp3.py

--- tasks/list_comp3.py
+++ tasks/list_comp3.py
@@ -1,9 +1,10 @@
+my_list=[["this is sentence 1", "this is sentence 2"],["this is sentence 2", "this is sentence 3"],]
+
 
 
 def list_comp3(input_list):
     """
     make this into a list comprehension
-
     example:
     >> list_comp3( [
                     ["this is sentence 1", "this is sentence 2"],
@@ -11,10 +12,10 @@ def list_comp3(input_list):
                     ])
     >> ['this', 'is', 'sentence', '1', 'this', 'is', 'sentence', '2', 'this', 'is', 'sentence', '2', 'this', 'is', 'sentence', '3']
     """
-    all_words = []
-    for sublist in input_list:
-        for string in sublist:
-            for word in string.split():
-                all_words.append(word)
+
+    all_words = [word for sublist in input_list for sentence in sublist for word in sentence.split()]
     
     return all_words
+
+
+print(list_comp3(my_list))
