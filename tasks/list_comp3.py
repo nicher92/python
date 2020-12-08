6c4406910f8ab7fb3696c5ee8233384b8e348d84
nicher <niclas.hertzberg@gmail.com>
2020-12-08 14:35:39 +0100

    hej

--- tasks/list_comp3.py
+++ tasks/list_comp3.py
@@ -1,5 +1,5 @@
 
-#test
+
 def list_comp3(input_list):
     """
     make this into a list comprehension
@@ -11,12 +11,29 @@ def list_comp3(input_list):
                     ])
     >> ['this', 'is', 'sentence', '1', 'this', 'is', 'sentence', '2', 'this', 'is', 'sentence', '2', 'this', 'is', 'sentence', '3']
     """
-    all_words = []
-    for sublist in input_list:
-        for string in sublist:
-            for word in string.split():
-                all_words.append(word)
-                
-     
+    #all_words = []
+    #for sublist in input_list:
+    #    for string in sublist:
+    #        for word in string.split():
+    #            all_words.append(word)
+    
+    
+    all_words = all_words = [word for sublist in input_list for sentence in sublist for word in sentence.split()]
+    
+    
+    
     
     return all_words
+
+
+
+
+list3 = ( [
+                    ["this is sentence 1", "this is sentence 2"],
+                    ["this is sentence 2", "this is sentence 3"],
+      ] )
+
+
+lista = list_comp3(list3)
+
+print(lista)
--- tasks/pad.py
+++ tasks/pad.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def pad(input_list):
     """
@@ -6,10 +6,28 @@ def pad(input_list):
 
     input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
 
-    return a matrix where 
-    1) matrix[:len(input_list[i])] == input_list[i]
+    return a matrix where
+    
+    1) matrix[i][:len(input_list[i])] == input_list[i]
     and
-    2) matrix[len(input_list[i]):] == [0,0, ..., 0]
+    #padding
+    2) matrix[i][len(input_list[i]):] == [0,0, ..., 0]
 
     """
-    pass
+    
+    maxlength = len(max(input_list, key=len))
+    matrix =[]
+    for item in input_list:
+        extra_zeroes = maxlength - len(item)
+        matrix.append(list(np.pad(item, (0, extra_zeroes), 'constant')))
+    
+    
+    return(np.array(matrix))
+
+
+
+
+
+
+input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
+print(pad(input_list))
