ef4de0bb5384f4093e1c00029990fd80e8d0bcd3
nicher <niclas.hertzberg@gmail.com>
2020-12-20 09:26:23 +0100

    first part done i think..

--- tasks/parse_xml.py
+++ tasks/parse_xml.py
@@ -21,3 +21,53 @@ ners = {
 
 NOTE! NER_TYPE is the label not the drug text/name
 """
+
+
+
+"""
+write a function called parse_xml() which loads and parses data/21728182.xml. The data contains NER labeled sentences.
+
+The output of the parse_xml() should be a two dicts structured in the following way:
+
+sentences = {
+            sentence_id:"sentence",
+            ...
+            }
+
+ners = {
+            sentence_id:[
+                         ((char_start, char_end),  NER_TYPE),
+                        ...
+                        
+                        ]
+            ...
+        
+        }
+
+NOTE! NER_TYPE is the label not the drug text/name
+"""
+
+
+from xml.dom import minidom
+from xml.dom.minidom import parse
+import xml.etree.ElementTree as ET
+
+
+### only works when file is in the same directory as XML file ###
+tree = ET.parse("21728182.xml")
+root = tree.getroot()
+
+sentences = {}
+ners = {}
+
+
+#So far only the first part of assignment done
+#Not sure what to do in the second part yet, he wants sa dictionary of
+#sentence id as key, which is correct so far, but im not sure what value he wants
+for elem in root:
+	sentences[elem.attrib.get("id")] = elem.attrib.get("text")
+	for subelem in elem:
+		ners[elem.attrib.get("id")] = subelem.attrib.get("text")
+
+
+
